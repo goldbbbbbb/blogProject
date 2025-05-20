51f0e13c08aa6fc04efc427e10f30cc3f9b9396d
goldbbbbbb <80026057+goldbbbbbb@users.noreply.github.com>
2025-05-20 20:50:42 +0800

    Update auth.login.intergration.test.js

--- server/__test__/integration/auth.login.intergration.test.js
+++ server/__test__/integration/auth.login.intergration.test.js
@@ -58,10 +58,13 @@ describe('POST /register', () => {
         const successUsername = `loginTestUser_123`;
         const successPassowrd = '!Password123';
 
-        testDb.collection('users').insertOne({
+        await testDb.collection('users').insertOne({
             username: successUsername,
             password: successPassowrd
         })  
+
+        const insertedUser = await testDb.collection('users').findOne({ username: successUsername });
+        expect(insertedUser).not.toBeNull(); // 驗證找到使用者        
         
         const response = await request(app)
             .post('/api/login')

fa940d7471be72ab15f8bcb663989cebb0f89956
goldbbbbbb <80026057+goldbbbbbb@users.noreply.github.com>
2025-05-20 20:44:34 +0800

    add jwt_secret to git for test

--- server/src/repositories/authRepository.js
+++ server/src/repositories/authRepository.js
@@ -64,7 +64,6 @@ module.exports = {
     const usersCollection = db.collection('users');
     const user = await usersCollection.findOne({ username });
     const JWT_SECRET = process.env.JWT_SECRET;
-    console.log(JWT_SECRET);
     const token = jwt.sign(
       {
           userId: user._id,
